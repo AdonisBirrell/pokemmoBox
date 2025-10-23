aedd9a6dcf81f34f29c0e06ab85e0725e3706d99
Adonis Birrell <fhngfctthjgfhj@myinifdkothrud.com>
2025-10-23 15:15:00 +0800

    1

--- netlify/functions/getEvents.js
+++ netlify/functions/getEvents.js
@@ -1,11 +1,101 @@
-const Parser = require("rss-parser");
-const parser = new Parser();
+// gatsby-node.js
 
-exports.handler = async function (event, context) {
-    const data = await parser.parseURL("https://forums.pokemmo.com/index.php?/forum/36-official-events.xml/&member=440918&key=62f3187f80bafc5b7e856a3227756bdc")
+const fetch = require('node-fetch');
+const { getItemInfo, getPokemmoID } = require('./src/utils/items');
+import itemsData from './src/data/pokemmo/item_lookup.json'
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-    };
+
+const NODE_TYPE = "Pokemmo"
+
+const slugify = string => string.toString()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+
+const getItemName = (id) => {
+    const item = itemsData[id.toString()];
+    const dummy = {
+        "en": "Item Error - Fix In Progress",
+        "cn": "Item Error - Fix In Progress",
+        "de": "Item Error - Fix In Progress",
+        "fr": "Item Error - Fix In Progress",
+        "it": "Item Error - Fix In Progress",
+        "es": "Item Error - Fix In Progress",
+        "ja": "Item Error - Fix In Progress",
+        "tw": "Item Error - Fix In Progress"
+    }
+    if (!item) return dummy;
+
+    return item.name;
+}
+
+const getItemDescription = (id) => {
+    const item = itemsData[id.toString()];
+    const dummy = {
+        "en": "Item Error - Fix In Progress",
+        "cn": "Item Error - Fix In Progress",
+        "de": "Item Error - Fix In Progress",
+        "fr": "Item Error - Fix In Progress",
+        "it": "Item Error - Fix In Progress",
+        "es": "Item Error - Fix In Progress",
+        "ja": "Item Error - Fix In Progress",
+        "tw": "Item Error - Fix In Progress"
+    }
+    if (!item) return dummy;
+
+    const cleanedDescription = {};
+    for (const [lang, text] of Object.entries(item.description)) {
+        cleanedDescription[lang] = text.replace(/\\n/g, ' ');
+    }
+
+    return cleanedDescription;
+}
+
+exports.sourceNodes = async ({ actions, createContentDigest, createNodeId }) => {
+    const { createNode } = actions;
+
+    const response = await fetch(
+        `https://apis.fiereu.de/pokemmoprices/v1/items` // https://pokemmoprices.com/api/v2/items/all
+    );
+    const data = await response.json();
+
+    data.forEach((item) => {
+        let item_pokemmo_id = getPokemmoID(item.item_id)
+        if (!item_pokemmo_id) {
+            return false
+        }
+        const itemName = getItemName(item.item_id)
+        // 移除了导致页面创建失败的检查
+        const slug = slugify(itemName['en'])
+
+
+        item = {
+            ...item,
+            n: getItemName(item.item_id),
+            d: getItemDescription(item.item_id),
+            ...getItemInfo(item_pokemmo_id),
+            slug: slug,
+            _id: item_pokemmo_id
+        }
+
+        createNode({
+            ...item,
+            id: createNodeId(`${NODE_TYPE}-${item.item_id}`),
+            parent: null,
+            children: [],
+            context: {
+                i: item.item_id,
+                slug: slug
+            },
+            internal: {
+                type: NODE_TYPE,
+                content: JSON.stringify(item),
+                contentDigest: createContentDigest(item)
+            }
+        });
+    });
 };
